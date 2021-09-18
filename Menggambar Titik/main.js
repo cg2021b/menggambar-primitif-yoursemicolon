c6ef50900d43ef09273ba843accd82d44a0c9ad0
Amanda Rozi Kurnia <amandarozikurnia@gmail.com>
2021-09-18 14:58:48 +0700

    Create WebGLProgram

--- Menggambar Titik/main.js
+++ Menggambar Titik/main.js
@@ -18,11 +18,33 @@ function main() {
     var fragmentShaderCode = `
     void main() {
         gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        
-    }`
+    }`;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
+    // jadikan package berisi data-data
+    var shaderProgram = gl.createProgram();
+
+    // attach vertex dan fragment
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    // link program
+    gl.linkProgram(shaderProgram);
+
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        var info = gl.getProgramInfoLog(shaderProgram);
+        throw `Could not compile WebGL program. \n\n ${info}` // template literals
+    }
+
+    // use the program
+    gl.useProgram(shaderProgram);
+
+    // clear canvas
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINT, 0, 1);
 }
